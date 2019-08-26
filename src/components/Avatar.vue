ff0da998b1372a5385772c755e71f7abe777c9cd
Carlos Bonadeo <cdbonadeo@afip.gob.ar>
2019-08-26 16:54:22 -0300

    Avatar and Reply components

---
+++ src/components/Avatar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="avatar">
+    <img :src="src">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: { type: String, required: true }
+  }
+};
+</script>
+
+<style scoped>
+.avatar {
+  border-radius: 50%;
+  width: 45px;
+  height: 45px;
+  margin-right: 10px;
+  overflow: hidden;
+  background: #FFF;
+}
+</style>
---
+++ src/components/Reply.vue
@@ -0,0 +1,69 @@
+<template>
+  <transition name="slide-down">
+        <div class="reply bg-special" v-if="show" :class="{ 'in-message': inMessage }">
+            <div class="reply-header">
+                <strong>Reply to {{ message.user.name.first }}</strong>
+                <div 
+                    v-if="!inMessage"
+                    class="button button-icon" 
+                    @click="$emit('close')"
+                >
+                    <eva-icon name="close-outline" height="18" fill="white" />
+                </div>
+            </div>
+            <div class="reply-content">
+                <div v-for="(message, i) in message.text" :key="i">{{ message }}</div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    props: {
+        message: { required: true },
+        inMessage: { type: Boolean, default: false },
+        show: { type: Boolean, default: false }
+    }
+
+}
+</script>
+
+<style>
+.reply {
+    font-size: .8rem;
+}
+
+.reply.in-message {
+    background: none;
+    border-radius: .25rem;
+    margin-bottom: .5rem;
+    background: #ff3a4922;
+}
+
+.reply .reply-header {
+    color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: .5rem .5rem .25rem;
+}
+
+.reply.in-message .reply-header {
+    color: #333;
+}
+
+.reply .reply-content {
+    max-height: 75px;
+    background: rgba(255, 255, 255, 0.6);
+    margin: 0rem .5rem .5rem;
+    padding: .5rem;
+    border-radius: .25rem;
+    overflow: auto;
+}
+
+.reply.in-message .reply-content {
+    background: none;
+}
+
+</style>
