89526ca16716277257d927ce825596f54a75d9d2
Carlos Bonadeo <cdbonadeo@afip.gob.ar>
2019-08-26 17:01:11 -0300

    Change deploy settings

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:cdmoro/chat.git master:gh-pages
+# git push -f git@github.com:cdmoro/chat.git master:gh-pages
+git push -f https://github.com/cdmoro/chat.git master:gh-pages
 
 cd -
