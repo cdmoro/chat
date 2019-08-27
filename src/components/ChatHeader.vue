d671197399bd420bdfdb31f75b275a1cbc0e4fc6
Carlos Bonadeo <carlosbonadeo@gmail.com>
2019-08-26 22:06:21 -0300

    Isolate components

---
+++ src/components/ChatHeader.vue
@@ -0,0 +1,42 @@
+<template>
+    <header class="bg-special">
+        <avatar v-if="buddy" :src="buddy.picture.thumbnail"/>
+        <div class="title">{{ buddyTitle }}</div>
+    </header>
+</template>
+
+<script>
+import Avatar from "./Avatar"
+
+export default {
+    props: {
+        buddy: { type: Object, default: null }
+    },
+    components: {
+        Avatar
+    },
+    computed: {
+        buddyTitle() {
+            if (this.buddy === null)
+                return 'Loading...'
+            else 
+                return `${this.buddy.name.first} ${this.buddy.name.last}`
+        }
+    }
+}
+</script>
+
+<style>
+.chat header {
+    padding: 0.5rem;
+    height: 45px;
+    display: flex;
+    align-items: center;
+}
+
+.chat .title {
+    font-weight: bold;
+    color: white;
+    flex-grow: 1;
+}
+</style>
---
+++ src/components/NewMessagesAlert.vue
@@ -0,0 +1,24 @@
+<template>
+  <transition name="scale">
+      <button
+        v-show="show"
+        class="button circle new-messages"
+        title="You have new messages!"
+        @click="$emit('click')"
+      >
+        <eva-icon name="message-circle" animation="pulse" fill="white"/>
+      </button>
+    </transition>
+</template>
+
+<script>
+export default {
+    props: {
+        show: { type: Boolean, default: false }
+    }
+}
+</script>
+
+<style>
+
+</style>
